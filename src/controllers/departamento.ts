6fcabcb5547fb330fe119b24381f512e0cbf756d
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-27 16:20:54 -0400

    fix: crud - edicao

--- src/controllers/departamento.ts
+++ src/controllers/departamento.ts
@@ -11,7 +11,7 @@ const index = async (req: Request, res: Response) => {
 const create = async (req: Request, res: Response) => {
   if (req.route.methods.get) {
     res.render('departamento/create', {
-      csrf: req.csrfToken,
+      csrf: req.csrfToken(),
     });
   } else {
     const departamento = req.body;
@@ -23,7 +23,7 @@ const create = async (req: Request, res: Response) => {
       res.render('departamento/create', {
         departamento,
         errors: e.errors,
-        csrf: req.csrfToken,
+        csrf: req.csrfToken(),
       });
     }
   }
@@ -33,20 +33,29 @@ const read = async (req: Request, res: Response) => {};
 
 const update = async (req: Request, res: Response) => {
   if (req.route.methods.get) {
-    res.render(`/departamento/update/${req.params.id}`, {
-      csrf: req.csrfToken,
+    res.render('departamento/edit', {
+      csrf: req.csrfToken(),
     });
   } else {
     const departamento = req.body;
+    const departamentoId = req.params.id;
     try {
-      await Departamentos.create(departamento);
+      const updatedDepartamento = await Departamentos.findByPk(departamentoId);
+      if (!updatedDepartamento) {
+        return res
+          .status(404)
+          .render('error', { message: 'Departamento não encontrado' });
+      }
+
+      await updatedDepartamento.update(departamento);
+
       res.redirect('/departamento');
-    } catch (e: any) {
-      console.log(e);
-      res.render(`/departamento/update/${req.params.id}`, {
+    } catch (error: any) {
+      console.log(error);
+      res.render(`departamento/edit`, {
         departamento,
-        errors: e.errors,
-        csrf: req.csrfToken,
+        errors: error.errors,
+        csrf: req.csrfToken(),
       });
     }
   }
